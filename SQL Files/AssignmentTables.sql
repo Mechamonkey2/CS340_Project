6402e663adabb38d248e954aa2c7d3b7b111337b
Sammyall <60229555+Sammyall@users.noreply.github.com>
2022-05-20 12:45:54 -0700

    Update AssignmentTables.sql

--- SQL Files/AssignmentTables.sql
+++ SQL Files/AssignmentTables.sql
@@ -1,6 +1,6 @@
 
 
-create table Boot_Stats(
+create table Boot_Stat(
 	bootStatID int(16) auto_increment unique not null primary key,
 	physicalDefense int(8) not null,
 	magicalDefense int(8) not null,
@@ -9,7 +9,7 @@ create table Boot_Stats(
 	rarity varchar(64) not null
 );
 
-create table Legging_Stats(
+create table Legging_Stat(
 	leggingStatID int(16) auto_increment unique not null primary key,
 	physicalDefense int(8) not null,
 	magicalDefense int(8) not null,
@@ -18,7 +18,7 @@ create table Legging_Stats(
 	rarity varchar(64) not null
 );
 
-create table Chest_Piece_Stats(
+create table Chest_Piece_Stat(
 	chestPieceStatID int(16) auto_increment unique not null primary key,
 	physicalDefense int(8) not null,
 	magicalDefense int(8) not null,
@@ -27,7 +27,7 @@ create table Chest_Piece_Stats(
 	rarity varchar(64) not null
 );
 
-create table Helemt_Stats(
+create table Helmet_Stat(
 	helmetStatID int(16) auto_increment unique not null primary key,
 	physicalDefense int(8) not null,
 	magicalDefense int(8) not null,
@@ -36,51 +36,48 @@ create table Helemt_Stats(
 	rarity varchar(64) not null
 );
 
+create table Armors(
+	objectID int(16),
+	armorID  int(16) unique not null,
+	description varchar(128) not null,
+	
+	foreign key (objectID) references Gears(objectID)
+);
+
 create table Boots(
-	armorID int(16) auto_increment unique not null primary key,
+	armorID int(16),
 	description varchar(128) not null,
 	bootStatID int(16),
 
 	foreign key (bootStatID) references Boot_Stat(bootStatID),
+	foreign key (armorID) references Armors(armorID)
 );
 
 create table Leggings(
-	armorID int(16) auto_increment unique not null primary key,
+	armorID int(16),
 	description varchar(128) not null,
 	leggingStatID int(16),
 
 	foreign key (leggingStatID) references Legging_Stat(leggingStatID),
+	foreign key (armorID) references Armors(armorID)
 );
 
 create table Chest_Pieces(
-	armorID int(16) auto_increment unique not null primary key,
+	armorID int(16),
 	description varchar(128) not null,
 	chestPieceStatID int(16),
 
 	foreign key (chestPieceStatID) references Chest_Piece_Stat(chestPieceStatID),
+	foreign key (armorID) references Armors(armorID)
 );
 
 create table Helmets(
-	armorID int(16) auto_increment unique not null primary key,
+	armorID int(16),
 	description varchar(128) not null,
 	helmetStatID int(16),
 
 	foreign key (helmetStatID) references Helmet_Stat(helmetStatID),
-);
-
-
-
-
-
-create table Armors(
-	objectID int(16) auto_increment unique not null primary key,
-	armorID  int(16),
-	description varchar(128) not null,
-
-	foreign key (armorID) references Helmets(armorID),
-	foreign key (armorID) references Chest_Pieces(armorID),
-	foreign key (armorID) references Leggings(armorID),
-	foreign key (armorID) references Boots(armorID)
+	foreign key (armorID) references Armors(armorID)
 );
 
 
@@ -108,52 +105,47 @@ create table Sword_Stat (
 	rarity varchar(64) not null
 );
 
+create table Weapons (
+	objectID int(16),
+	weaponID int(16) unique not null,
+	description varchar(128),
+	
+	foreign key (objectID) references Gears(objectID)
+);
+
+
 create table Bows (
-	weaponID int(16) auto_increment unique not null primary key,
+	weaponID int(16),
 	description varchar(128) not null,
 	bowStatID int(16),
 
-	foreign key (bowStatID) references Bow_Stat(bowStatID)
+	foreign key (bowStatID) references Bow_Stat(bowStatID),
+	foreign key (weaponID) references Weapons(weaponID)
 );
 
 
 create table Staffs (
-	weaponID int(16) auto_increment unique not null primary key,
+	weaponID int(16),
 	description varchar(128) not null,
 	staffStatID int(16),
 
-	foreign key (StaffStatID) references Staff_Stat(StaffStatID)
+	foreign key (StaffStatID) references Staff_Stat(StaffStatID),
+	foreign key (weaponID) references Weapons(weaponID)
 );
 
 create table Swords (
-	weaponID int(16) auto_increment unique not null primary key,
+	weaponID int(16),
 	description varchar(128) not null,
 	swordStatID int(16),
 
-	foreign key (swordStatID) references Sword_Stat(swordStatID)
+	foreign key (swordStatID) references Sword_Stat(swordStatID),
+	foreign key (weaponID) references Weapons(weaponID)
 );
-
---create tables for multiple fk ids
-
-create table Weapons (
-	objectID int(16) auto_increment unique not null primary key,
-	weaponID int(16),
-	description varchar(128),
-
-	foreign key (weaponID) references Swords(weaponID),
-	foreign key (weaponID) references Staffs(weaponID),
-	foreign key (weaponID) references Bows(weaponID)
-);
-
 
 create table Gears(
 	gearID int(16) auto_increment unique not null primary key,
-	objectID int(16),
-	equipmentTypeID varchar(128) not null,
-
-
-	foreign key (objectID) references Armors(objectID),
-	foreign key (objectID) references Weapons(objectID)
+	objectID int(16) unique not null,
+	equipmentType varchar(128) not null,
 );
 
 create table Mana_Potions (
@@ -201,11 +193,22 @@ create table Player_Inventory (
 INSERT INTO Bow_Stat(bowStatID, attack, bonus, durability,rarity)
 VALUES ('696', '150', '5% bonus damage', '100','Legendary');
 
-INSERT INTO Staff_Stat(staffStatID, attack, bonus, durability,rarity)
+INSERT INTO Staff_Stat(staffStatID, magic, bonus, durability,rarity)
 VALUES ('151', '150', '5% bonus damage', '100','Legendary');
 
 INSERT INTO Sword_Stat(swordStatID, attack, bonus, durability,rarity)
 VALUES ('132', '150', '5% bonus damage', '100','Legendary');
+
+
+INSERT INTO Weapons (objectID, weaponID, description)
+VALUES ('1','1',"A ranged weapon");
+
+INSERT INTO Weapons (objectID, weaponID, description)
+VALUES ('2','2',"A magical stick");
+
+INSERT INTO Weapons (objectID, weaponID, description)
+VALUES ('3','3',"A versatile weapon");
+
 
 INSERT INTO Bows(weaponID, description, bowStatID)
 VALUES ('1', 'Silver Bow', '696');
@@ -216,12 +219,14 @@ VALUES ('2', 'Great Branch Staff', '151');
 INSERT INTO Swords(weaponID, description, swordStatID)
 VALUES ('3', 'Silver Sword', '132');
 
-INSERT INTO Weapons (objectID, weaponID, description)
-VALUES (NULL,1,"A ranged weapon");
+INSERT INTO Gears (gearID, objectID, equipmentType)
+VALUES ('1','1','Bow');
 
-INSERT INTO Weapons (objectID, weaponID, description)
-VALUES (NULL,2,"A magical stick");
+INSERT INTO Gears (gearID, objectID, equipmentType)
+VALUES ('2','2','Staff');
 
-INSERT INTO Weapons (objectID, weaponID, description)
-VALUES (NULL,3,"A versatile weapon");
+INSERT INTO Gears (gearID, objectID, equipmentType)
+VALUES ('3','3','Sword');
+
+
 
