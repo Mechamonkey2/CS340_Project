53f9de630547f0620deb803e9572d943f2206270
Duy Pham <notouchieduy@yahoo.com>
2022-06-03 22:12:38 -0700

    WEAPON DML
    WEAPON DML

--- SQL Files/data_manipulation.sql
+++ SQL Files/data_manipulation.sql
@@ -13,3 +13,14 @@ WHERE rarity = 'Legendary';
 /*DELETE First Table increment from Item_IDs of Consumable table*/
 DELETE FROM Consumables WHERE consumableID = (SELECT consumableID from Item_IDs WHERE consumableID = '1');
 DELETE FROM Item_IDs WHERE consumableID = '1';
+
+/*Show Weapon_Stats*/
+SELECT weaponStatID, attack FROM Weapon_Stats;
+
+/*UPDATE attack from Weapon_Stats of Legendary*/
+UPDATE Weapon_Stats SET attack = '250'
+WHERE rarity = 'Legendary';
+
+/*UPDATE Common weapon*/
+UPDATE Weapon_Stats SET attack = '15'
+WHERE magic = '3'
