ea8d26f8f42e8f8fd911dcaf4fb65f9bc2ca1fb9
Sammyall <60229555+Sammyall@users.noreply.github.com>
2022-06-03 22:41:35 -0700

    Last update

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_westerhs',
-  password        : 'iris',
-  database        : 'cs340_westerhs'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 module.exports.pool = pool;
